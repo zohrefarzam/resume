4df949b558db3ab3cadd50d0e88fd3775d2e09a7
zohre <zohrefarzam@gmail.com>
2020-04-06 06:28:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data.json'
 import './App.css';
-
+import { SocialIcon } from 'react-social-icons';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navigation"></div>
+    <div className='fullPage'>
+      <div>
+        <h1>{data.title}</h1>
+      </div>
+      <div>
+      <h2>{data.subtitle}</h2>
+      </div>
+      {
+      Object.keys(data.links).map(key=>
+        {
+          return(<SocialIcon url={data.links[key]} />)
+        })
+      }
+    </div>
+    <div className='fullPage'>
+      <h3>
+        {data.sections[0].title}
+      </h3>
+      <p>
+      {data.sections[0].items[0].content} 
+      </p>
+    </div>
+    <div className='fullPage'></div>
     </div>
   );
 }
